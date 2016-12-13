c9c88f1ef49fcb8de79922a68e3b4fc697873c68
Ignacio Alvarez <ignacioalvarez92@gmail.com>
2016-12-12 23:33:34 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+
+const foregroundColor = '#fff';
+const backgroundColor = 'rgba(31, 43, 49, .96)';
+const darkerBackgroundColor = 'rgba(28, 40, 46, .30)';
+const red = '#ff5c57';
+const green = '#5af78e';
+const yellow = '#fefe7a';
+const blue = '#57b7ff';
+const magenta = '#ff6ac1';
+const cyan = '#98edff';
+const white = '#fff';
+
+exports.decorateConfig = (config) => Object.assign({}, config, {
+  backgroundColor,
+  foregroundColor,
+  borderColor: darkerBackgroundColor,
+  cursorColor: white,
+  colors: {
+    black: backgroundColor,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
+    lightBlack: '#686868',
+    lightRed: red,
+    lightGreen: green,
+    lightYellow: yellow,
+    lightBlue: blue,
+    lightMagenta: magenta,
+    lightCyan: cyan,
+    lightWhite: foregroundColor
+  },
+  css: `
+    ${config.css}
+    .hyper_main {
+      border: none !important;
+    }
+    .header_header {
+      background-color: ${darkerBackgroundColor} !important;
+    }
+    .tabs_borderShim {
+      border-color: transparent !important;
+    }
+    .tab_tab {
+      border: 0;
+    }
+    .tab_textActive {
+      border-bottom: 2px solid ${green};
+    }
+  `
+});
